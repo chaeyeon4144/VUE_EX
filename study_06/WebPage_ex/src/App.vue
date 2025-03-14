ae429768139a782bd10ddf71debd5e6269511359
chaeyeon4144 <odora4144@gmail.com>
2025-03-14 12:16:02 +0900

    컴포넌트를 사용해서 사이트 예제 연습

---
+++ study_06/WebPage_ex/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Header from "./components/Header.vue";
+import MainContent from "./components/MainContent.vue";
+import Footer from "./components/Footer.vue";
+</script>
+
+<template>
+  <Header />
+  <MainContent />
+  <Footer />
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100vh;
+}
+</style>
